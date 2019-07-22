f5b3cc1a1de1664a68ef201864170e2ec141352a
qubitter <terminalvelocit3@gmail.com>
2019-07-22 16:03:40 -0700

    Added images and more code

--- code/experiment/main.m
+++ code/experiment/main.m
@@ -1,25 +1,28 @@
 clear all;
 close all;
+KbName('UnifyKeyNames');
+ListenChar(-1);
+
 
 exp = input('Please enter the experiment code. ', 's');
 
 %% Code is processed as follows:
 %  First digit: 
 %      single (0), unbiased ensemble (1), or biased ensemble (2)
-%  Second digit: 
-%      if single, then race && gender - Asian m (0), Black m (1), Latino m
-%      (2), White m (3) - female is male + 4
-%      if ensemble, then first race && gender - Asian m (0), Black m (1), Latino m
-%      (2), White m (3) - female is male + 4
-%  Third digit: 
-%      Time per image/set in tens of ms
-%  Fourth digit:
+%  Second digit:
 %      Fives of trials in hex
-%  Fifth digit: 
+%  Third digit: 
 %      Trait - attractiveness (0), punctuality (1), afraid (2),
 %      angry (3), disgusted (4), dominant (5), feminine (6), happy (7),
 %      masculine (8), sad (9), surprised (a), threatening (b), trustworthy
 %      (c), unusual (d), babyface (e), educated (f)
+%  Fourth digit: 
+%      if single, 0 (can be used for testing purposes)
+%      if ensemble, time per image/set in tens of ms
+%  Fifth digit:
+%      if single, 0 (can be used for testing purposes)
+%      if ensemble, then first race && gender - Asian m (0), Black m (1), Latino m
+%      (2), White m (3) - female is male + 4
 %  Sixth digit:
 %      if single, 0 (can be used for testing purposes)
 %      if ensemble, then second race && gender - Asian m (0), Black m (1), Latino m
@@ -45,27 +48,65 @@ traits = {'Attractive', 'Punctual', 'Afraid', 'Angry', 'Disgusted', 'Dominant', 
 if (str2double(exp(1)) == 0); single = true; else; single = false; end
 ensemble = ~single;
 
-if (ensemble); if (str2double(exp(1)) == 1); bias = false; elseif (str2double(exp(1)) == 2); bias = true; end; end
+if (ensemble); if (str2double(exp(1)) == 1); bias = false; elseif (str2double(exp(1)) == 2); bias = true; else; bias = false; end; end
 
-if (ensemble); expString = 'sets of 6'; else; expString = 'pairs of'; end
+if (ensemble); expString = 'sets of 6 images, each of which will be followed by a pair of'; else; expString = 'pairs of'; end
 
 % Race and Gender
-firstRaceGender = personcodes(str2double(exp(2))+1, :);
-if (ensemble); secondRaceGender = personcodes(str2double(exp(6))+1, :); else; secondRaceGender = NaN; end
+
+if (ensemble); firstRaceGender = personcodes(str2double(exp(5))+1, :); secondRaceGender = personcodes(str2double(exp(6))+1, :); end
 
 % Trial time
-trialTime = str2double(exp(3)).*10;
+if (ensemble); trialTime = str2double(exp(4)).*10; end
 
 % Number of trials
-numTrials = (hex2dec(exp(4))+1).*10;
+numTrials = (hex2dec(exp(3))+1).*10;
 
 % Trait
-trait = traits{hex2dec(exp(5))+1};
+trait = traits{hex2dec(exp(4))+1};
 
 %% Introduction
 
-DrawFormattedText(window, ['Welcome to the experiment. \n \n You will be shown a series of ' expString ' images. \n \n You will be asked to choose the image that most corresponds with a certain trait. \n \n A break will be taken after 50 trials, or you can cancel the experiment at any time by pressing Escape. \n \n Press any key to continue. '], 'center', 'center', 0, 50);
+DrawFormattedText(window, ['Welcome to the experiment. \n \n You will be shown a series of ' expString ' images. \n \n You will be asked to choose the image that most corresponds with a certain trait. \n \n A break will be taken after every 50 trials, or you can cancel the experiment at any time by pressing Escape. \n \n Press any key to continue. '], 'center', 'center', 0, 50);
 Screen('Flip', window);
 KbWait();
 
+%% Create stimulus list
+
+stimuliorder = randperm(600);
+stimuli = [];
+
+
+if (single)
+    stimuli = zeros([1200 1]);
+    for stimNum = stimuliorder
+       tmp = [];
+       if (floor(a/100) ~= 0); tmp = num2str(stimNum); elseif (floor(a/10) ~= 0); tmp = ['0' num2str(stimNum)]; else; tmp = ['00' num2str(stimNum)]; end
+       stimuli((2.*stimNum)-1) = imread(['../stimuli/noisy/rcic_im_1_00' tmp '_ori.jpg']);
+       stimuli(2.*stimNum) = imread(['../stimuli/noisy/rcic_im_1_00' tmp '_inv.jpg']);
+    end
+elseif (~bias)
+    
+else
+    
+end
+
+
+%% Start experiment
+KbQueueCreate(1);
+while KbCheck; end
+KbQueueStart(1);
+
+for trail = 1:numTrials
+    [pressed, firstPress] = KbQueueCheck(1);
+    if firstPress(KbName('ESCAPE')); break; end
+    if (single)
+        
+        
+    elseif ~bias
+        
+    else
+        
+    end
+end    
 Screen('Close', window);
