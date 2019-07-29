6de4da1b601eb21409719df422dd33ebc5bfeb8e
siddharthnandy <53063086+siddharthnandy@users.noreply.github.com>
2019-07-29 11:41:17 -0700

    Update rcicr_code.R

--- code/ci_generation/rcicr_code.R
+++ code/ci_generation/rcicr_code.R
@@ -14,11 +14,15 @@ rdata <- 'rcic_seed_1_time_Jul_22_2019_11_54.Rdata'
 
 # Load response data
 # NOTE: 'response_conan19r.csv' is used for one subject code; use appropriate names for each subject; see below
-responsedata <- read.csv('response_QIU Shi23R.csv')
+responsedata <- read.csv('response_conan19r_copy.csv')
 
 # change code as it is warranted
 stimulus <- responsedata$Image
+# Use this line when generating noise CI
 response <- responsedata$Noise
+# Use this line when generating anti-noise CI
+# response <- responsedata$Anti 
 
 # Batch generate classification images by trait
-ci <- generateCI2IFC(stimulus, response, baseimage, rdata)
+# ci <- generateCI2IFC(stimulus, response, baseimage, rdata)
+ci <- generateCI(stimulus, response, baseimage, rdata)
